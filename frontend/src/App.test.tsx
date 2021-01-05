ac68ba9191831c6a9fa9a9b60e3ddf50f21ce38d
Baylee Schmeisser <github@baylee.dev>
2021-01-04 22:07:38 -0800

    🧪 Update react test for new  link.

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders home', () => {
   render(<App />);
-  const linkElement = screen.getByText(/click me/i);
+  const linkElement = screen.getByText('Create Room');
   expect(linkElement).toBeInTheDocument();
 });
