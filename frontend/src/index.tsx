81b9c2e86f2b9a74efb679a5adca78ac7ed9145e
Baylee Schmeisser <github@baylee.dev>
2021-08-01 11:43:24 -0700

    🎉 We could have been using SCSS this whole time?!

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import './index.css';
+import './index.scss';
 import './scss/material-dashboard.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
