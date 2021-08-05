4fdd1748764e6d2b940619061fba381393ab85f4
Amjith Ramanujam <amjith@netflix.com>
2021-08-04 22:33:57 -0700

    Move static to project root.

--- backend/entrypoint.py
+++ backend/entrypoint.py
@@ -9,5 +9,5 @@ command.upgrade(alembic_cfg, "head")
 
 # Start server
 
-uvicorn.run("backend.main:app", fd=0, log_level="debug")
-# uvicorn.run("backend.main:app", host="127.0.0.1",port=9000, log_level="debug")
+# uvicorn.run("backend.main:app", fd=0, log_level="debug")
+uvicorn.run("backend.main:app", host="127.0.0.1", port=9000, log_level="debug")
--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ from fastapi.templating import Jinja2Templates
 app = FastAPI()
 app.include_router(api_router, prefix="/api")
 app.mount("/static", StaticFiles(directory="templates/static"), name="static")
-app.mount("/api/static", StaticFiles(directory="backend/static"), name="static")
+app.mount("/api/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 # Set all CORS enabled origins
