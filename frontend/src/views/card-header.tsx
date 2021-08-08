01f22d0dbc3b350502b1847221daa0101644c7ff
Baylee Schmeisser <github@baylee.dev>
2021-08-07 20:15:45 -0700

    🚧 fixup security warning

--- frontend/src/views/card-header.tsx
+++ frontend/src/views/card-header.tsx
@@ -38,7 +38,7 @@ export const CardHeader: FC<CardHeaderProps> = (props: CardHeaderProps) => {
             </Card.Text>
             <Card.Text>
               Source:
-              <a target="_blank" href={card.definitionSource}>
+              <a rel="noreferrer" target="_blank" href={card.definitionSource}>
                 {getDomain(card.definitionSource)}
               </a>
             </Card.Text>
