76c398a7176aceb3ebe5772a6562a59978961149
Amjith Ramanujam <amjith@netflix.com>
2021-08-15 13:54:00 -0700

    Improvements to loadtests.

--- loadtest.py
+++ loadtest.py
@@ -6,26 +6,27 @@ import os
 from coolname import generate_slug
 from random import uniform, choice
 
-PY_ENV = os.environ.get('PY_ENV')
-ROOM_COUNT = int(os.environ.get('ROOM_COUNT'))
-ROOM_SIZE = int(os.environ.get('ROOM_SIZE'))
-ROOM_TICKS = int(os.environ.get('ROOM_TICKS'))
-PY_DOMAIN = '127.0.0.1:8000'
-if PY_ENV == 'prod':
-    PY_DOMAIN = 'ineedempathy.com'
+PY_ENV = os.environ.get("PY_ENV")
+ROOM_COUNT = int(os.environ.get("ROOM_COUNT"))
+ROOM_SIZE = int(os.environ.get("ROOM_SIZE"))
+ROOM_TICKS = int(os.environ.get("ROOM_TICKS"))
+PY_DOMAIN = "127.0.0.1:8000"
+if PY_ENV == "prod":
+    PY_DOMAIN = "ineedempathy.com"
+
 
 class EmpathyRoom:
     @classmethod
-    def build(cls, room_size):
+    def build(cls, room_size, room_id):
         try:
-            url = 'http://' + PY_DOMAIN + '/api/rooms'
-            room_name = json.loads(requests.post(url).text)['name']
-            print('OPEN | Room: ' + room_name)
+            url = "http://" + PY_DOMAIN + "/api/rooms"
+            room_name = json.loads(requests.post(url).text)["name"]
+            # print('OPEN | Room: ' + room_name)
             klass = cls(room_name, room_size)
-            klass.fill_room()
+            klass.fill_room(room_id)
             return klass
         except requests.exceptions.RequestException as e:
-            print('ERROR: ')
+            print("ERROR: ")
             print(e)
 
     def __init__(self, room_name, room_size):
@@ -35,7 +36,7 @@ class EmpathyRoom:
 
     # complete a random action
     def tick(self):
-        print('TICK | Room: ' + self.room_name)
+        print("TICK | Room: " + self.room_name)
         choice(self.users).toggle(int(uniform(0, 80)))
         self.receive()
 
@@ -48,10 +49,16 @@ class EmpathyRoom:
             user.close()
             self.receive()
 
-    def fill_room(self):
+    def fill_room(self, room_id):
         for x in range(0, self.room_size):
-            self.users.append(RoomUser.build(self.room_name))
-            self.receive()
+            user = RoomUser.build(self.room_name)
+            if user.connect():
+                self.users.append(user)
+                self.receive()
+            else:
+                print(f"DROP | User: {user}")
+        print(f"CREATED | Room: {room_id} - {self.room_name} Users: {len(self.users)}")
+
 
 class RoomUser:
     @classmethod
@@ -61,32 +68,38 @@ class RoomUser:
     def __init__(self, room_name, user_name):
         self.room_name = room_name
         self.user_name = user_name
-        print('OPEN | Room: ' + self.room_name + '; User: ' + self.user_name)
-        self.url = 'ws://' + PY_DOMAIN + '/api/rooms/' + \
-            self.room_name + '/users/' + \
-            self.user_name + '.ws'
-        self.ws = websocket.create_connection(self.url)
+        # print('OPEN | Room: ' + self.room_name + '; User: ' + self.user_name)
+        self.url = "ws://" + PY_DOMAIN + "/api/rooms/" + self.room_name + "/users/" + self.user_name + ".ws"
+
+    def connect(self):
+        try:
+            self.ws = websocket.create_connection(self.url)
+        except Exception:
+            return False
+        return True
 
     def close(self):
-        print('CLOSE | Room: ' + self.room_name + '; User: ' + self.user_name)
+        print("CLOSE | Room: " + self.room_name + "; User: " + self.user_name)
         return self.ws.close(1005)
 
     def receive(self):
         if self.ws.connected:
-            result =  self.ws.recv()
-            #print("Received '%s'" % result)
+            self.ws.recv()
+            # print("Received '%s'" % result)
 
     def toggle(self, card_id):
-        self.ws.send('{"toggleCard": ' + str(card_id) + '}')
+        self.ws.send('{"toggleCard": ' + str(card_id) + "}")
+
 
 def run(room_count, room_ticks, room_size):
     rooms = []
 
     for x in range(0, room_count):
         time.sleep(0.1)
-        room = EmpathyRoom.build(room_size=room_size)
+        room = EmpathyRoom.build(room_size=room_size, room_id=x)
         rooms.append(room)
 
+    time.sleep(3)
     for x in range(0, room_ticks):
         time.sleep(0.05)
         for room in rooms:
@@ -94,5 +107,6 @@ def run(room_count, room_ticks, room_size):
 
     for room in rooms:
         room.close()
+
 
 run(room_count=ROOM_COUNT, room_ticks=ROOM_TICKS, room_size=ROOM_SIZE)
