82ce6a7c5508d3dc574e2b7dc6dec162d8baf008
Baylee Schmeisser <github@baylee.dev>
2021-08-16 19:24:47 -0700

    Remove sentry. [skip ci]

--- frontend/src/config/index.tsx
+++ frontend/src/config/index.tsx
@@ -1,7 +1,6 @@
 export interface EnvConfig {
   BACKEND_URL: string;
   SOCKET_URL: string;
-  SENTRY_DSN: string;
 }
 export interface EnvConfigs {
   [key: string]: EnvConfig;
@@ -11,21 +10,17 @@ const configFor = (envString: string):EnvConfig => {
     production: {
       BACKEND_URL: 'http://ineedempathy.com/api',
       SOCKET_URL: 'ws://ineedempathy.com/api',
-      SENTRY_DSN: 'https://3355964559bb41f9a7d44b99c3f7123b@o948279.ingest.sentry.io/5897485',
     },
     development: {
       BACKEND_URL: 'http://127.0.0.1:8000/api',
       SOCKET_URL: 'ws://127.0.0.1:8000/api',
-      SENTRY_DSN: '',
     },
   };
   return confs[envString] || {
     BACKEND_URL: 'http://127.0.0.1:8000/api',
     SOCKET_URL: 'ws://127.0.0.1:8000/api',
-    SENTRY_DSN: '',
   };
 }
 
 export const BACKEND_URL = configFor(process.env.NODE_ENV).BACKEND_URL;
 export const SOCKET_URL = configFor(process.env.NODE_ENV).SOCKET_URL;
-export const SENTRY_DSN = configFor(process.env.NODE_ENV).SENTRY_DSN;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,18 +1,9 @@
 import React from 'react';
-import * as Sentry from "@sentry/react";
-import { Integrations } from "@sentry/tracing";
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.scss';
 import './scss/material-dashboard.scss';
 import App from './App';
-import { SENTRY_DSN } from './config';
-
-Sentry.init({
-  dsn: SENTRY_DSN,
-  integrations: [new Integrations.BrowserTracing()],
-  tracesSampleRate: 1.0,
-});
 
 ReactDOM.render(
   <Router>
